cc823fa10287bd0288bb48a0a791875481dd9358
Vikas Reddy <90479368+vikasreddy85@users.noreply.github.com>
2026-09-22 10:32:30 -0400

    Solve cosine-similarity

---
+++ cosine-similarity/cosine-similarity.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def cosine_similarity(a: list, b: list) -> float:
+    """
+    Returns the cosine similarity as a Python float.
+    """
+    # Write code here
+    a = np.asarray(a, dtype=float)
+    b = np.asarray(b, dtype=float)
+    a_norm = np.linalg.norm(a)
+    b_norm = np.linalg.norm(b)
+    if a_norm == 0 or b_norm == 0:
+        return 0.0
+    return float(np.dot(a,b)) / float(np.dot(a_norm, b_norm))
+    
+    pass
